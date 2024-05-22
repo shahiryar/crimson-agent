fd772bbe5536eeea302bcf2af19d9b288e09699d
shahiryar <shahiryarsaleem@gmail.com>
2024-05-22 12:52:51 +0500

    Use session in the app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit_chat import message
 from utils import *
 from agent import Agent
+from session import Session
 
 @st.cache_resource
 def create_agent():
@@ -11,17 +12,28 @@ def create_agent():
     agent = Agent(intents_classifier, sentiment_analyser)
     return agent
 
+@st.cache_resource
+def create_session():
+    return Session(create_agent())
+
+
+current_session = create_session()
+
 st.session_state["messages"] = [] if not ("messages" in st.session_state.keys()) else st.session_state["messages"]
-agent = create_agent()
+current_session.set_state("messages", st.session_state["messages"])
+#agent = create_agent()
 
 user_input = st.chat_input("Your Message", key="1234") #TODO generate clock bound random key instead
 if user_input:
     
-    agent_reply = agent.process_input(user_input)["reply"]
+    agent_reply = current_session.interact(user_input)["reply"]
 
 
     st.session_state.messages.append({"role": "user", "content": user_input})
+    #current_session.session_state["messages"].append({"role": "user", "content": user_input})
+
     st.session_state.messages.append({"role": "agent", "content": agent_reply})
+    #current_session.session_state["messages"].append({"role": "agent", "content": agent_reply})
     
 for i, msg in enumerate(st.session_state.messages):
     is_user = msg["role"] == "user"
